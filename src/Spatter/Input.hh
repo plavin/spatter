1d5ef1d958912c712fcee9784f6c7970f535797c
Patrick Lavin <prlavin@sandia.gov>
2024-03-29 07:49:05 -0600

    default to openmp or cuda if they are compiled, serial otherwise

--- src/Spatter/Input.hh
+++ src/Spatter/Input.hh
@@ -213,7 +213,7 @@ size_t remap_pattern(aligned_vector<size_t> &pattern, const size_t boundary) {
 }
 
 int parse_input(const int argc, char **argv, ClArgs &cl) {
-  cl.backend = "serial";
+  cl.backend = "";
   cl.aggregate = false;
   cl.atomic = false;
   cl.compress = false;
@@ -441,6 +441,18 @@ int parse_input(const int argc, char **argv, ClArgs &cl) {
       usage(argv[0]);
       return -1;
     }
+  }
+
+  // Set default backend if one was not specified
+  if (backend.compare("") == 0) {
+    backend = "serial";
+    // Assume only one of USE_CUDA and USE_OPENMP can be true at once
+#ifdef USE_OPENMP
+    backend = "openmp";
+#endif
+#ifdef USE_CUDA
+      backend = "cuda";
+#endif
   }
 
   cl.backend = backend;
