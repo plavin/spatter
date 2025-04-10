973554ae7b320db86d7288aa9862ad11fddd863e
King <tking88@instinct.crnch.gatech.edu>
2025-04-09 21:21:24 -0400

    fix merge conflict on project version

--- src/Spatter/Input.hh
+++ src/Spatter/Input.hh
@@ -20,9 +20,9 @@
 #include <omp.h>
 #endif
 
-#include "Configuration.hip.hh"
+#include "Configuration.hh"
 #include "JSONParser.hh"
-#include "PatternParser.hip.hh"
+#include "PatternParser.hh"
 #include "SpatterTypes.hh"
 
 namespace Spatter {
